26f69a72f598150aef2e32506ae0ef6ce20ea1ac
zhanshenzhengtu <87885882+zhanshenzhengtu@users.noreply.github.com>
2021-08-25 14:23:02 +0800

    Add files via upload

---
+++ hotalbookingsystem/src/main/java/com/sys/hotalbookingsystem/HotalbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.sys.hotalbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotalbookingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotalbookingsystemApplication.class, args);
+    }
+
+}
---
+++ hotalbookingsystem/src/test/java/com/sys/hotalbookingsystem/HotalbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.hotalbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotalbookingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
